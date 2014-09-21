3f7ebfb06078f4631c7dd65e7869cc7ab335afe9
Derek Wu <derekwu@Dereks-MacBook-Pro.local>
2014-09-21 18:11:52 -0400

    Finished resetting dice roll

--- main.js
+++ main.js
@@ -18,7 +18,8 @@ var test_board = [[1,2,3,4,5],
                   [5,4,3,2,1]]
 function Dice() {
     /* 
-    Standard dice orientation: Key is top value, Array is [back, right, front, left]
+    Standard dice orientation: Key is front value (the value that is added to the board tile)
+                               Array is [top, right, bottom, left]
     */
     var DICE_ORIENTATION_DICT = {1:[5, 3, 2, 4],
                                  2:[1, 3, 6, 4],
@@ -26,16 +27,34 @@ function Dice() {
                                  4:[5, 1, 2, 6],
                                  5:[6, 3, 1, 4],
                                  6:[5, 4, 2, 3]}
+    var front;
+    var back;
     var top;
     var bottom;
     var left;
     var right;
-    var front;
-    var back;
+
     resetDice();
 
-    /* set 6 variables randomly, using dict*/
-    function resetDice() {}
+    /* Rolls the die: set 6 variables randomly, using dict*/
+    function resetDice() {
+        front = Math.floor((Math.random() * 6) + 1);
+        back = 7 - front;
+
+        standardOrientation = DICE_ORIENTATION_DICT[front];
+        orientationIndex = Math.floor(Math.random()*4);
+        orientationArray = [0, 0, 0, 0]; // top, right, bottom, left
+
+        for (i = 0; i < 4; i++) {
+            orientationArray[i] = standardOrientation[orientationIndex];
+            orientationIndex = (orientationIndex + 1) % 4;
+        }
+
+        top = orientationArray[0];
+        right = orientationArray[1];
+        bottom = orientationArray[2];
+        left = orientationArray[3];
+    }
 
     /* these four functions will update the 6 variables appropriately */
     function rollUp(){
@@ -70,7 +89,19 @@ function Dice() {
         bottom = temp;
     }
 
-    /* we also need getter functions */
+    /*
+    Get the value of the front side of the dice
+    */
+    function getFrontValue() {
+        return front;
+    }
+
+    /*
+    Get the values of the side of the dice (top, right, bottom, left)
+    */
+    function getSideValues() {
+        return [top, right, bottom, left];
+    }
 
 
 }
